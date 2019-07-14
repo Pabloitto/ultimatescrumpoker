0242afd592f50cca822faef28fac76c99f5f355b
Pablo Itto <pablo.itto@unosquare.com>
2019-07-13 18:13:31 -0700

    Fixing issues with reconnection

--- server/src/services/estimation-room/index.js
+++ server/src/services/estimation-room/index.js
@@ -1,80 +1,137 @@
 const EstimationRoomService = ({
+  io,
   client,
-  rooms = {}
+  RoomModel
 }) => {
-  const init = roomId => {
-    let room = rooms[roomId]
-    if (!room) {
-      room = {
-        roomId: roomId,
-        users: {}
-      }
-      rooms[roomId] = room
+  const cleanUp = (room) => {
+    room.users = room.users.filter(user => {
+      return !!io.sockets.sockets[user.clientId]
+    })
+    return room.save()
+  }
+
+  const init = async roomId => {
+    let room = await RoomModel.findOne({ roomId })
+    if (room) {
+      await cleanUp(room)
+    } else {
+      room = await RoomModel.create({ roomId })
     }
+
     client.join(roomId)
     client.emit('update', JSON.stringify(room))
   }
 
-  const join = (roomId, clientJoined) => {
-    const room = rooms[roomId]
-    room.users[client.id] = {
-      name: clientJoined.name,
-      estimation: clientJoined.estimation,
-      flipped: clientJoined.flipped
+  const join = async (roomId, clientJoined) => {
+    const room = await RoomModel.findOne({ roomId })
+
+    const user = room.users.find(user => user.name === clientJoined.name)
+
+    if (!user) {
+      room.users.push({
+        name: clientJoined.name,
+        estimation: clientJoined.estimation,
+        flipped: clientJoined.flipped,
+        clientId: client.id
+      })
+    } else {
+      user.estimation = clientJoined.estimation
+      user.flipped = clientJoined.flipped
+      user.clientId = client.id
     }
-    client.emit('update', JSON.stringify(room))
-    client.broadcast.to(room.roomId).emit('update', JSON.stringify(room))
+
+    await room.save()
+
+    const roomPayload = JSON.stringify(room)
+
+    client.emit('update', roomPayload)
+    client.broadcast.to(room.roomId).emit('update', roomPayload)
   }
 
-  const estimate = (roomId, points) => {
-    const room = rooms[roomId]
-    room.users[client.id].estimation = points
-    room.users[client.id].flipped = false
-    client.emit('update', JSON.stringify(room))
-    client.broadcast.to(room.roomId).emit('update', JSON.stringify(room))
+  const estimate = async (roomId, points) => {
+    const room = await RoomModel.findOne({ roomId })
+
+    const user = room.users.find((user) => user.clientId === client.id)
+
+    user.estimation = points
+    user.flipped = false
+
+    await room.save()
+
+    const roomPayload = JSON.stringify(room)
+
+    client.emit('update', roomPayload)
+    client.broadcast.to(room.roomId).emit('update', roomPayload)
   }
 
-  const flipCards = roomId => {
-    const room = rooms[roomId]
-    for (const u in room.users) {
-      if (room.users[u]) {
-        room.users[u].flipped = true
+  const flipCards = async roomId => {
+    const room = await RoomModel.findOne({ roomId })
+
+    room.users.forEach((user) => {
+      user.flipped = true
+    })
+
+    await room.save()
+
+    const roomPayload = JSON.stringify(room)
+
+    client.emit('update', roomPayload)
+    client.broadcast.to(room.roomId).emit('update', roomPayload)
+  }
+
+  const cleanEstimations = async roomId => {
+    const room = await RoomModel.findOne({ roomId })
+
+    room.users.forEach((user) => {
+      user.flipped = false
+      user.estimation = ''
+    })
+
+    await room.save()
+
+    const roomPayload = JSON.stringify(room)
+
+    client.emit('update', roomPayload)
+    client.broadcast.to(room.roomId).emit('update', roomPayload, true)
+  }
+
+  const leaveRoom = async () => {
+    const room = await RoomModel.updateOne({
+      $pull: {
+        users: {
+          clientId: client.id
+        }
       }
-    }
-    client.emit('update', JSON.stringify(room))
-    client.broadcast.to(room.roomId).emit('update', JSON.stringify(room))
+    })
+
+    const roomPayload = JSON.stringify(room)
+
+    client.emit('update', roomPayload)
+    client.broadcast.to(room.roomId).emit('update', roomPayload)
   }
 
-  const cleanEstimations = roomId => {
-    const room = rooms[roomId]
-    for (const u in room.users) {
-      if (room.users[u]) {
-        room.users[u].flipped = false
-        room.users[u].estimation = ''
-      }
-    }
-    client.emit('update', JSON.stringify(room))
-    client.broadcast.to(room.roomId).emit('update', JSON.stringify(room), true)
-  }
+  const setDeckInRoom = async (roomId, deck) => {
+    const room = await RoomModel.findOne({ roomId })
 
-  const leaveRoom = () => {
-    let room = {}
-    for (var r in rooms) {
-      if (rooms[r].users && rooms[r].users[client.id]) {
-        rooms[r].users[client.id] = null
-        room = rooms[r]
-      }
-    }
-    client.emit('update', JSON.stringify(room))
-    client.broadcast.to(room.roomId).emit('update', JSON.stringify(room))
-  }
-
-  const setDeckInRoom = (roomId, deck) => {
-    const room = rooms[roomId]
     room.deck = deck
-    room.deckAleadySelected = true
+
+    await room.save()
+
     client.emit('removeDeckSelection', room.deck)
+
     client.broadcast.to(room.roomId).emit('removeDeckSelection', room.deck)
+  }
+
+  const syncConnection = async (roomId, userName) => {
+    const room = await RoomModel.findOne({ roomId })
+    if (!room) return
+    const user = room.users.find(user => user.name === userName)
+    if (user && user.clientId !== client.id) {
+      console.log(`Reconnecting client from ${user.clientId} to ${client.id}`)
+      user.clientId = client.id
+      client.join(roomId)
+      await room.save()
+    }
   }
 
   return {
@@ -84,7 +141,8 @@ const EstimationRoomService = ({
     flipCards,
     cleanEstimations,
     leaveRoom,
-    setDeckInRoom
+    setDeckInRoom,
+    syncConnection
   }
 }
 
